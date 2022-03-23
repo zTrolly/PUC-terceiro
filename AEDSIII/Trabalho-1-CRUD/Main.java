96e3ffa1f65a18c281d62472f52ed5263e652218
Breno Lopes <brenolcdev@gmail.com>
2022-03-23 10:47:29 -0300

    UPDATE

--- AEDSIII/Trabalho-1-CRUD/Main.java
+++ AEDSIII/Trabalho-1-CRUD/Main.java
@@ -1,7 +1,21 @@
 import java.io.IOException;
 import java.util.Scanner;
 
+
 public class Main {
+  public void limpatela() throws IOException{
+    String[] cmd;
+//checking for OS
+if(System.getProperty("os.name").contains("win")){
+	cmd=new String[]{"cmd", "/c", "cls"};
+}else{
+	cmd=new String[]{"cls"};
+}
+//clearing the screen
+ProcessBuilder pb=new ProcessBuilder(cmd);
+  pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
+  Process process =  pb.start();
+  }
   public static void main(String[] args){
     short opcao = 50;
 		Scanner leitor = new Scanner(System.in);
